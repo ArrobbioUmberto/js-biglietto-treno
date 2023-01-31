a221dd1ff2244e4e9f17cfd53e8c4d77268e0aad
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-01-31 15:36:33 +0100

    FEAT scheme for structure

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+
+// 1. numero di chilometri che vuole percorrere 
+// 2. età del passeggero 
+// 3. calcolare il prezzo del biglietto 
+//    tenendo in considerazione che: 
+    // costa 0.21 euro al km 
+    // se minorennes sconto del 20%
+    // se over 65 sconto del 40 %
+    // output del prwzzo finale va messo con max due decimali per indicare i centesimi del prezzo 
+
+
