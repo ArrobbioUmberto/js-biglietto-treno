2d6cdb7638a80c9fa0d42d1074ea4e26bdfffd07
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-01-31 15:54:11 +0100

    FEAT set the ticket price for <18, >65

--- app.js
+++ app.js
@@ -11,3 +11,21 @@
     // output del prwzzo finale va messo con max due decimali per indicare i centesimi del prezzo 
 
 
+let numberKm = parseFloat( prompt('Quanti chilometri devi percorrere?'))
+console.log('questi sono i km che vuole percorrere', numberKm)
+let passengerAge = parseInt (prompt('Quanti anni hai ?'))
+console.log('questa è l\'età del nostro passeggero', passengerAge)
+
+const ticketPrice = 0.21
+
+let totalKm = numberKm * ticketPrice
+
+if (passengerAge < 18){
+    totalKm = (numberKm * ticketPrice) * 0.8
+} else if (passengerAge > 64){
+    totalKm = (numberKm * ticketPrice) * 0.6
+}
+else {
+    totalKm = numberKm * ticketPrice
+}
+console.log('questo è il prezzo del biglietto', totalKm)
